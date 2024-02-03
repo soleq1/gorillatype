5f3fda6cd0ac99f95eb03d503e66865291048dc6
Eddie Zieleniewski <solesskidder1@gmail.com>
2024-02-03 00:17:24 -0500

    Fix First load

--- src/components/CoreVue.vue
+++ src/components/CoreVue.vue
@@ -66,7 +66,9 @@ export default{
   },
     mounted(){
         //Force focused Input
-        this.$refs.inputRef.focus();
+        this.$refs.inputRef.focus();  
+        console.log(localStorage.getItem('wordCount'));
+        
 
         
         this.words = wordData.default   
@@ -105,10 +107,14 @@ export default{
      wordCountVal(){
       const val = localStorage.getItem('wordCount')
       // console.log(val);
-      if (val != this.wordCount){
+      if (val == null){
+        return
+      }
+      else if (val != this.wordCount ){
         this.wordCount = val
         this.handleRestart()
       }
+      
       
      },
       randomWords() {
@@ -127,7 +133,7 @@ export default{
   }
 
   //Shift words into random order then slice off 25
-  const combinedWords = shuffledWords.map(word => word.word).slice(0,this.wordCount).join(' ')
+  const combinedWords = shuffledWords.map(word => word.word).slice(0,this.wordCount || 25).join(' ')
   this.displayedWords = combinedWords
   
 },
@@ -257,6 +263,7 @@ today = mm + '-' + dd + '-' + yyyy;
           Date: today,
           WPM: this.WPM,
           Accuracy: this.acc,
+          Version:this.wordCount,
 
         }
       console.log(today);
@@ -354,6 +361,8 @@ localStorage.setItem('records', JSON.stringify(filteredRecord));
           <p>Wpm:{{ record.WPM.toFixed(2) }}</p>
           <p>Accuracy: {{ record.Accuracy.toFixed(2) }}</p>
           <p>{{ record.Date }}</p>
+          <p v-if="record.Version == 25">Long</p>
+          <p v-else-if='record.Version == 10'>Short</p>
           <button @click="handleDelete(index)" class="recordDelete"><svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
   <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z"/>
   <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z"/>
